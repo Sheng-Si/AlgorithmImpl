83045f7fcac0c9f5eeae676aa87e8705989ae447
zhaowenyan <zhaowenyan@mmbang.com>
2019-05-08 13:54:11 +0800

    新增两个来自于leetcode的算法

---
+++ other/medianOfTwoSortedArray.py
@@ -0,0 +1,26 @@
+"""
+Median of Two Sorted Arrays
+
+Create by kyle 2019.05.08
+"""
+
+from __future__ import division
+
+
+def find_median_sorted_arrays(nums1, nums2):
+    a, b = sorted((nums1, nums2), key=len)
+    m, n = len(a), len(b)
+    after = (m + n - 1) // 2
+    lo, hi = 0, m
+    while lo < hi:
+        i = (lo + hi) // 2
+        if after - i - 1 < 0 or a[i] >= b[after - i - 1]:
+            hi = i
+        else:
+            lo = i + 1
+    i = lo
+    nextfew = sorted(a[i:i + 2] + b[after - i:after - i + 2])
+    return (nextfew[0] + nextfew[1 - (m + n) % 2]) / 2
+
+
+print(find_median_sorted_arrays([1], [0]))
---
+++ other/twoSum.py
@@ -0,0 +1,20 @@
+"""
+给定一个待选数组和一个目标数,在待选数组中寻找哪两个数之和等于目标数
+
+Create by kyle 2019.05.08
+"""
+
+
+def two_sum(arr, target):
+    tmp = {}
+    for index, n in enumerate(arr):
+        diff = target - n
+        cache = tmp.get(diff)
+        if cache:
+            return [n, arr[cache]]
+        else:
+            tmp[n] = index
+    return []
+
+
+print(two_sum([1, 2, 3, 4, 1, 1, 4, 5, 6, 1, 23, 5123, 123, 15, 1], 100))
